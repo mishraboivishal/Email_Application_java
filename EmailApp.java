609a96b8eda8a9d408863207f772a7fc71d4ff6b
VISHAL MISHRA <73197685+mishraboivishal@users.noreply.github.com>
2023-11-26 22:55:34 +0530

    Add files via upload

---
+++ EmailApp.java
@@ -0,0 +1,9 @@
+package com.email;
+
+ public class EmailApp {
+	public static void main(String[] args) {
+		// Create an instance of EmailAccount class to run the email application
+		EmailAccount.main(args);
+
+	}
+}
